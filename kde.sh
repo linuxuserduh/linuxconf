35c1e46a46b8dc2d84cbe0f02b22e4b10dde17f2
bababooey <115456907+linuxuserduh@users.noreply.github.com>
2026-03-15 10:33:17 +0800

    added fedora install script

--- kde.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo apt install -y kde-plasma-desktop modemmanager- bluedevil- plasma-discover- kdeconnect- kwalletmanager- bolt- kup-backup- plasma-vault-
--- tweaks.sh
+++ tweaks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# From CachyOS settings
+
+# allow feral gamemode renice limitation to 4
+echo '@gamemode - nice -4' | sudo tee /etc/security/limits.d/10-gamemode.conf > /dev/null
+
+## CachyOS udev rules
 # I/O Scheduler Rules
 echo -e '# HDD
 ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
@@ -10,7 +14,10 @@ ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="
 # NVMe SSD
 ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"' | sudo tee /etc/udev/rules.d/60-ioschedulers.rules > /dev/null
 
-# Sysctl config
+# hdparm rules
+echo -e 'ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", ATTRS{id/bus}=="ata", RUN+="/usr/bin/hdparm -B 254 -S 0 /dev/%k"' | sudo tee /etc/udev/rules.d/69-hdparm.rules > /dev/null
+
+# Sysctl
 echo -e '# This action will speed up your boot and shutdown, because one less module is loaded. Additionally disabling watchdog timers increases performance and lowers power consumption
 # Disable NMI watchdog
 kernel.nmi_watchdog = 0
